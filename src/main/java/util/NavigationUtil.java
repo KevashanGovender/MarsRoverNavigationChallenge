2f0dd69485638061f62432d182d6d2ce12c6bc81
kevashangovender <kevashan.govender@standardbank.co.za>
2019-02-14 15:59:07 +0200

    T004 Updated NavigationUtil
    * This commit will :
    
        * Added navigation logic for EAST, SOUTH and WEST
        * Refactor the class to use switch statements instead of if statements as its slightly cleaner

--- src/main/java/util/NavigationUtil.java
+++ src/main/java/util/NavigationUtil.java
@@ -4,6 +4,8 @@ import model.Direction;
 import model.Instruction;
 import model.Rover;
 
+import static model.Instruction.*;
+
 public class NavigationUtil {
 
     private Rover rover;
@@ -15,20 +17,74 @@ public class NavigationUtil {
     }
 
     public void move(Instruction instruction) {
-        if (roverCurrentDirection == Direction.NORTH && instruction == Instruction.M){
-            rover.setYPos(moveForward(rover.getYPos()));
-        }
-
-        if(roverCurrentDirection == Direction.NORTH && instruction == Instruction.L){
-            rover.setDirection(Direction.WEST);
-        }
-
-        if(roverCurrentDirection == Direction.NORTH && instruction == Instruction.R){
-            rover.setDirection(Direction.EAST);
+        switch (roverCurrentDirection) {
+            case NORTH: handleNorth(instruction);
+            break;
+            case EAST: handleEast(instruction);
+            break;
+            case SOUTH: handleSouth(instruction);
+            break;
+            case WEST: handleWest(instruction);
         }
     }
 
     private int moveForward(int currentYPos) {
         return currentYPos + 1;
+    }
+
+    private int moveRight(int currentXPos){
+        return currentXPos + 1;
+    }
+
+    private int moveBack(int currentYPos){
+        return currentYPos - 1;
+    }
+
+    private int moveLeft(int currentXPos){
+        return currentXPos - 1;
+    }
+
+    private void handleNorth(Instruction instruction){
+        switch (instruction){
+            case M : rover.setYPos(moveForward(rover.getYPos()));
+            break;
+            case L: rover.setDirection(Direction.WEST);
+            break;
+            case R: rover.setDirection(Direction.EAST);
+            break;
+        }
+    }
+
+    private void handleEast(Instruction instruction){
+        switch (instruction){
+            case M : rover.setXPos(moveRight(rover.getXPos()));
+                break;
+            case L: rover.setDirection(Direction.NORTH);
+                break;
+            case R: rover.setDirection(Direction.SOUTH);
+                break;
+        }
+    }
+
+    private void handleSouth(Instruction instruction){
+        switch (instruction){
+            case M : rover.setYPos(moveBack(rover.getYPos()));
+                break;
+            case L: rover.setDirection(Direction.EAST);
+                break;
+            case R: rover.setDirection(Direction.WEST);
+                break;
+        }
+    }
+
+    private void handleWest(Instruction instruction){
+        switch (instruction){
+            case M : rover.setXPos(moveLeft(rover.getXPos()));
+                break;
+            case L: rover.setDirection(Direction.SOUTH);
+                break;
+            case R: rover.setDirection(Direction.NORTH);
+                break;
+        }
     }
 }
--- src/test/java/util/NavigationUtilTest.java
+++ src/test/java/util/NavigationUtilTest.java
@@ -59,4 +59,125 @@ public class NavigationUtilTest {
         assertEquals(1, rover.getYPos());
     }
 
+    @Test
+    public void testThatIfRoverDirectionIsEast_AndInstructionIsM_RoverXPosIncreasesBy1(){
+        rover.setDirection(Direction.EAST);
+        rover.setXPos(1);
+
+        navigationUtil.setRover(rover);
+        navigationUtil.move(Instruction.M);
+
+        assertEquals(2, rover.getXPos());
+    }
+
+    @Test
+    public void testThatIfRoverDirectionIsEast_AndInstructionIsL_RoverDirectionIsN(){
+        rover.setDirection(Direction.EAST);
+        rover.setXPos(1);
+        rover.setYPos(1);
+
+        navigationUtil.setRover(rover);
+        navigationUtil.move(Instruction.L);
+
+        assertEquals(Direction.NORTH, rover.getDirection());
+        assertEquals(1, rover.getXPos());
+        assertEquals(1, rover.getYPos());
+    }
+
+    @Test
+    public void testThatIfRoverDirectionIsEast_AndInstructionIsR_RoverDirectionIsS(){
+        rover.setDirection(Direction.EAST);
+        rover.setXPos(1);
+        rover.setYPos(1);
+
+        navigationUtil.setRover(rover);
+        navigationUtil.move(Instruction.R);
+
+        assertEquals(Direction.SOUTH, rover.getDirection());
+        assertEquals(1, rover.getXPos());
+        assertEquals(1, rover.getYPos());
+    }
+
+    @Test
+    public void testThatIfRoverDirectionIsSouth_AndInstructionIsM_RoverYPosDecreasesBy1(){
+        rover.setDirection(Direction.SOUTH);
+        rover.setXPos(1);
+        rover.setYPos(1);
+
+        navigationUtil.setRover(rover);
+        navigationUtil.move(Instruction.M);
+
+        assertEquals(0, rover.getYPos());
+    }
+
+    @Test
+    public void testThatIfRoverDirectionIsSouth_AndInstructionIsL_RoverDirectionIsE(){
+        rover.setDirection(Direction.SOUTH);
+        rover.setXPos(1);
+        rover.setYPos(1);
+
+        navigationUtil.setRover(rover);
+        navigationUtil.move(Instruction.L);
+
+        assertEquals(Direction.EAST, rover.getDirection());
+        assertEquals(1, rover.getXPos());
+        assertEquals(1, rover.getYPos());
+    }
+
+    @Test
+    public void testThatIfRoverDirectionIsSouth_AndInstructionIsR_RoverDirectionIsW(){
+        rover.setDirection(Direction.SOUTH);
+        rover.setXPos(1);
+        rover.setYPos(1);
+
+        navigationUtil.setRover(rover);
+        navigationUtil.move(Instruction.R);
+
+        assertEquals(Direction.WEST, rover.getDirection());
+        assertEquals(1, rover.getXPos());
+        assertEquals(1, rover.getYPos());
+    }
+
+    @Test
+    public void testThatIfRoverDirectionIsWest_AndInstructionIsM_RoverXPosDecreasesBy1(){
+        rover.setDirection(Direction.WEST);
+        rover.setXPos(1);
+        rover.setYPos(1);
+
+        navigationUtil.setRover(rover);
+        navigationUtil.move(Instruction.M);
+
+        assertEquals(0, rover.getXPos());
+    }
+
+    @Test
+    public void testThatIfRoverDirectionIsWest_AndInstructionIsL_RoverDirectionIsS(){
+        rover.setDirection(Direction.WEST);
+        rover.setXPos(1);
+        rover.setYPos(1);
+
+        navigationUtil.setRover(rover);
+        navigationUtil.move(Instruction.L);
+
+        assertEquals(Direction.SOUTH, rover.getDirection());
+        assertEquals(1, rover.getXPos());
+        assertEquals(1, rover.getYPos());
+    }
+
+    @Test
+    public void testThatIfRoverDirectionIsSouth_AndInstructionIsR_RoverDirectionIsN(){
+        rover.setDirection(Direction.WEST);
+        rover.setXPos(1);
+        rover.setYPos(1);
+
+        navigationUtil.setRover(rover);
+        navigationUtil.move(Instruction.R);
+
+        assertEquals(Direction.NORTH, rover.getDirection());
+        assertEquals(1, rover.getXPos());
+        assertEquals(1, rover.getYPos());
+    }
+
+
+
 }
